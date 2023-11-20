68b4ea6424983f1118b756f1a2513acdb2126312
MJ Wojcik <michael-wojcik@users.noreply.github.com>
2023-11-20 22:30:00 +0000

    basic hello world route

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,16 +1,13 @@
 import express from "express";
-
 const app = express();
-
 // GET
 // PUT
 // POST
 // DELETE
-
 app.get("/hello", (req, res, next) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
-
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+    console.log("Server is running on port 5000");
 });
+//# sourceMappingURL=index.js.map
