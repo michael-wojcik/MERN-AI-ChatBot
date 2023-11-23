55602411c269ad71d9f1c3d128910f196f553295
MJ Wojcik <michael-wojcik@users.noreply.github.com>
2023-11-23 12:52:57 -0500

    update routes again

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+// middleware
 app.use(express.json());
 // GET
 // PUT
@@ -8,12 +9,17 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
     res.send("Hello World");
 });
-app.get("/hello", (req, res, next) => {
+app.put("/hello", (req, res, next) => {
+    console.log(req.body.name);
     res.send("Hello World");
 });
 app.post("/hello", (req, res, next) => {
     console.log(req.body.name);
     res.send("Hello World");
+});
+app.delete("/user/:id", (req, res, next) => {
+    console.log(req.params.id);
+    res.send(`User ${req.params.id} deleted`);
 });
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 
+// middleware
 app.use(express.json());
 
 // GET
@@ -10,18 +11,24 @@ app.use(express.json());
 // DELETE
 
 app.get("/", (req, res, next) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
-app.get("/hello", (req, res, next) => {
-  res.send("Hello World");
+app.put("/hello", (req, res, next) => {
+    console.log(req.body.name);
+    res.send("Hello World");
 });
 
 app.post("/hello", (req, res, next) => {
-  console.log(req.body.name);
-  res.send("Hello World");
+    console.log(req.body.name);
+    res.send("Hello World");
+});
+
+app.delete("/user/:id", (req, res, next) => {
+    console.log(req.params.id);
+    res.send(`User ${req.params.id} deleted`);
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
